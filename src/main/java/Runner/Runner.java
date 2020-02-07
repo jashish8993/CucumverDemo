2b560bff964cd333c0b29ed8dcb2907cd353c363
jashish8993 <ashishj@TEKMINDZ.COM>
2020-02-07 15:57:05 +0530

    add cucumber options

--- src/main/java/Runner/Runner.java
+++ src/main/java/Runner/Runner.java
@@ -6,9 +6,14 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-features = "src/main/java/Features"
-,glue= {"seleniumgluecode"}
-)
+		features = "src/main/java/Features",
+		glue= {"seleniumgluecode"},
+		dryRun= false, 
+		//dryRun=true-----it means that Cucumber will only check that every Step mentioned in the Feature File has corresponding code written in Step Definition file or not. 
+		monochrome=true,
+		//monochrome=true-----If it is set as true, it means that the console output for the Cucumber test are much more readable.
+		plugin={"pretty","html:Test-output"}
+		)
 
 public class Runner {
 
